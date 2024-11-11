7fb7ac3dce9b7d34d3d0f04f6eae668ae5da7223
Fields Marshall <fields.marshall@gmail.com>
2024-11-11 11:32:59 -0800

    initial website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    /* other integrations */
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: `@import "./src/styles/global.css";`,
+        },
+      },
+    },
+  },
+})
