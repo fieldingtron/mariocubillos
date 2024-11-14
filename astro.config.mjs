d8abc201b7c57f4fd6b248f1638f76f3ec539e01
Fields Marshall <fields.marshall@gmail.com>
2024-11-14 00:48:08 -0800

    whatsapp icon added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
 
 export default defineConfig({
   integrations: [
+    icon(),
     /* other integrations */
   ],
   vite: {
